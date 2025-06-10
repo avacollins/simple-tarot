d00f41c5c473e27e27a689b0792a831edd7f03d5
Ava Collins <info@avacollins.dev>
2025-06-10 19:03:42 -0400

    build(ui): use relative path for service worker on gh pages

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -3,14 +3,22 @@ import { initialize, mswLoader } from 'msw-storybook-addon';
 import { INITIAL_VIEWPORTS } from 'storybook/viewport';
 import type { Preview } from '@storybook/react-native-web-vite';
 
+let options = {};
+if (location.hostname === 'avacollins.github.io') {
+    options = {
+        serviceWorker: {
+            url: '/simple-tarot/mockServiceWorker.js'
+        },
+        onUnhandledRequest: 'bypass'
+    };
+}
+
+initialize(options);
 /*
  * Initializes MSW
  * See https://github.com/mswjs/msw-storybook-addon#configuring-msw
  * to learn how to customize it
  */
-initialize({
-    onUnhandledRequest: 'bypass'
-});
 
 const preview: Preview = {
     parameters: {
