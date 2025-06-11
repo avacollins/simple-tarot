4fbb910b413e45839cb6fbf7d0b2e6a3c4803008
Ava Collins <info@avacollins.dev>
2025-06-11 11:04:50 -0400

    therapy

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,8 +1,43 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { View } from 'react-native';
+
 export default function Home() {
+    const [yellow, setYellow] = useState(false);
+    const [red, setRed] = useState(false);
+    const [green, setGreen] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (red) {
+                setRed(false);
+                setGreen(true);
+
+                return;
+            }
+
+            if (yellow) {
+                setYellow(false);
+                setRed(true);
+
+                return;
+            }
+
+            if (green) {
+                setYellow(true);
+                setGreen(false);
+
+                return;
+            }
+
+            setRed(true);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [red, yellow, green]);
+
     return (
         <View
             style={{
@@ -10,10 +45,39 @@ export default function Home() {
                 justifyContent: 'center',
                 alignItems: 'center'
             }}>
-            <p>
-                Get started by editing&nbsp;
-                <code className="font-mono font-bold">app/page.tsx</code>
-            </p>
+            <View
+                style={{
+                    width: 100,
+                    height: 100,
+                    backgroundColor: 'red',
+                    borderRadius: 50,
+                    padding: 20,
+                    margin: 10,
+                    opacity: red ? 1 : 0.5
+                }}
+            />
+            <View
+                style={{
+                    width: 100,
+                    height: 100,
+                    backgroundColor: 'yellow',
+                    borderRadius: 50,
+                    padding: 20,
+                    margin: 10,
+                    opacity: yellow ? 1 : 0.5
+                }}
+            />
+            <View
+                style={{
+                    width: 100,
+                    height: 100,
+                    backgroundColor: 'green',
+                    borderRadius: 50,
+                    padding: 20,
+                    margin: 10,
+                    opacity: green ? 1 : 0.5
+                }}
+            />
         </View>
     );
 }
