12b23cd320d56ea8dc028684241daa0c19848a9f
Ava Collins <info@avacollins.dev>
2025-06-13 16:55:54 -0400

    feat: add connection to neo4j docker db

---
+++ apps/graph-api/README.md
@@ -0,0 +1,7 @@
+# Core API
+
+Graph orchestrated API provides the core API for the tarot and admin applications.
+
+### Graph Schema
+
+![Graph Schema](../../assets/graph-schema.png)
--- apps/graph-api/src/index.ts
+++ apps/graph-api/src/index.ts
@@ -1,13 +1,10 @@
 import { ApolloServer } from '@apollo/server';
-import { fileURLToPath } from 'url';
+import { Neo4jGraphQL } from '@neo4j/graphql';
 import { gql } from 'graphql-tag';
+import neo4j from 'neo4j-driver';
 import path from 'path';
 import { readFileSync } from 'fs';
 import { startStandaloneServer } from '@apollo/server/standalone';
-
-// Add these lines:
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const typeDefs = gql(
     readFileSync(path.resolve(__dirname, './schema.graphql'), {
@@ -15,10 +12,27 @@ const typeDefs = gql(
     })
 );
 
-async function startApolloServer() {
-    const server = new ApolloServer({ typeDefs });
-    const { url } = await startStandaloneServer(server);
-    console.log(`🚀 Server ready at ${url}`);
-}
+const NEO4J_URL = process.env.NEO4J_DB_URL || 'bolt://localhost:7687';
+const NEO4J_USER = process.env.NEO4J_AUTH_USER || 'neo4j';
+const NEO4J_PASSWORD = process.env.NEO4J_AUTH_PASSWORD || 'password';
 
-startApolloServer();
+const driver = neo4j.driver(NEO4J_URL, neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD));
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+(async () => {
+    try {
+        const schema = await neoSchema.getSchema();
+        const server = new ApolloServer({
+            schema
+        });
+        const { url } = await startStandaloneServer(server, {
+            context: async ({ req }) => ({ req }),
+            listen: { port: 4000 }
+        });
+        console.log(`🚀 Server ready at ${url}`);
+    } catch (error) {
+        console.error('Error creating schema:', error);
+        process.exit(1);
+    }
+})();
