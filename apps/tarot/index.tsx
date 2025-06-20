164aacd4d991b107b2a34d7106b6827186205fa1
Ava Collins <info@avacollins.dev>
2025-06-19 21:48:02 -0400

    feat(tarot): Adds apollo client

--- apps/tarot/index.tsx
+++ apps/tarot/index.tsx
@@ -1,7 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
 import App from './app/index';
+import React from 'react';
+import client from './apollo-client';
 import { registerRootComponent } from 'expo';
 
+const Root = () => (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Root);
