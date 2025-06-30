caf0244b04aa7022fdcdbca48f834e23b46d97d0
Ava Collins <info@avacollins.dev>
2025-06-30 15:32:01 -0400

    test(hooks): adds test for index exports and coverage reporter

--- packages/hooks/vitetest.config.mjs
+++ packages/hooks/vitetest.config.mjs
@@ -4,6 +4,18 @@ export default defineConfig({
     test: {
         environment: 'jsdom',
         globals: true,
+         coverage: {
+            provider: 'v8', // or 'c8', 'istanbul'
+            reporter: ['text', 'json', 'html'],
+            exclude: [
+                'node_modules/',
+                'dist/',
+                '**/*.d.ts',
+                '**/*.test.{ts,tsx}',
+                '**/*.spec.{ts,tsx}',
+                "**/*.mjs",
+            ]
+        }
     },
     esbuild: {
         target: 'ES2017'
