04fd771845316edbb5c7991c92c9ad691b6ca0bf
Ava Collins <info@avacollins.dev>
2025-07-01 19:20:54 -0400

    test: interaction tests on quick nav have flaky timing issues trying to mitigate

--- packages/ui/stories/tests/quick-nav.stories.tsx
+++ packages/ui/stories/tests/quick-nav.stories.tsx
@@ -1,18 +1,14 @@
 import { Meta, StoryObj } from '@storybook/react-native-web-vite';
 import QuickNav from '../organisms/quick-nav';
 import React from 'react';
-import {
-    expect,
-    fireEvent,
-    screen,
-    within,
-    waitFor,
-    waitForElementToBeRemoved
-} from 'storybook/test';
+
+import { forceReloadDecorator } from './force-reload-decorator';
+import { expect, fireEvent, screen, within, waitFor } from 'storybook/test';
 
 const meta: Meta<typeof QuickNav> = {
     title: 'Organisms/QuickNav',
-    component: QuickNav
+    component: QuickNav,
+    decorators: [forceReloadDecorator]
 } satisfies Meta<typeof QuickNav>;
 
 export default meta;
@@ -20,7 +16,8 @@ export default meta;
 type Story = StoryObj<typeof QuickNav>;
 
 export const QuickNavOpenTest: Story = {
-    play: async ({ step }) => {
+    play: async ({ mount, step }) => {
+        await mount(<QuickNav />);
         const canvas = within(screen.getByTestId('quick-nav-container'));
 
         await step('Click toggle button', async () => {
@@ -28,6 +25,10 @@ export const QuickNavOpenTest: Story = {
             expect(toggleButton).toBeVisible();
             fireEvent.click(toggleButton);
             fireEvent.animationEnd(toggleButton);
+        });
+
+        await step('Check if quick nav is open', async () => {
+            console.log('Checking if quick nav is open');
             await waitFor(() =>
                 expect(canvas.getByTestId('quick-nav-profile-action')).toBeVisible()
             );
@@ -40,7 +41,11 @@ export const QuickNavOpenTest: Story = {
             await waitFor(() =>
                 expect(canvas.getByTestId('quick-nav-home-action')).toBeVisible()
             );
+        });
+
+        await step('Click toggle button again', async () => {
             const profileAction = canvas.getByTestId('quick-nav-profile-action');
+            console.log('Clicking profile action');
             fireEvent.click(profileAction);
             fireEvent.animationEnd(profileAction);
             await waitFor(() =>
